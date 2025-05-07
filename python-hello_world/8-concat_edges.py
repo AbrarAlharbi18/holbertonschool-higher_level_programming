62481ae905935a03dc9ca8ccd56a5a9367b1b441
AbrarAlharbi18 <Abrarsaadalharbi@gmail.com>
2025-05-07 10:55:16 +0000

    task6

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[39:67] + str[107:113] + str[0:6])
+print(__import__('this').s[39:67] + __import__('this').s[107:113] + __import__('this').s[0:6])
