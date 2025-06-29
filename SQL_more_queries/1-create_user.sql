9cfc098d7f49fc8cdb0c7a51192426205c73130c
AbrarAlharbi18 <Abrarsaadalharbi@gmail.com>
2025-06-29 17:05:37 +0000

    new task

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,3 @@
--- creates a database and user
--- DDL COMMAND CREATE IS USED
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON  hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+-- creates a new user
+CREATE  USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
